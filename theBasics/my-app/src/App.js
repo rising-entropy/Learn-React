23858c218b16f27027e692b4c6a1a8784d3e500d
Devang Kamble <risingentropy20@gmail.com>
2021-03-23 17:51:42 +0530

    structure

---
+++ theBasics/my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello There!</h1>
+    </div>
+    // React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello There!'))
+  );
+}
+
+export default App;
